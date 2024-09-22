44e287788f50597adb0f2c37868f0ca75b8d41fd
Shafkat-2022745 <2022745@iub.edu.bd.com>
2024-09-22 18:03:42 +0600

    first commit

---
+++ header.php
@@ -0,0 +1,19 @@
+<header class="header">
+
+   <div class="flex">
+
+      <a href="#" class="logo"></a>
+
+      <nav class="navbar">
+         <a href="index.html">Home</a>
+         <a href="admin.php">add products</a>
+         <a href="products.php">view products</a>
+         <a href="adminreg.php">New Admin</a>
+      </nav>
+
+
+      <div id="menu-btn" class="fas fa-bars"></div>
+
+   </div>
+
+</header>
